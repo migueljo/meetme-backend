530d679ba419741cfc94871e322f903270e68390
Miguel <migueljomartinez@gmail.com>
2023-07-29 22:27:09 -0500

    Adding event message handlers

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,41 @@ io.on('connection', (socket) => {
   socket.on('disconnect', (reason) => {
     console.log('Disconnected reason:', reason);
   });
+
+  socket.on('video-offer', (data: VideoOfferData) => {
+    console.log('video-offer', data);
+    // Send the offer to the target
+  });
+
+  socket.on('video-answer', (data: VideoAnswerData) => {
+    console.log('video-answer', data);
+    // Send the answer to the target
+  });
+
+  socket.on('new-ice-candidate', (data: IceCandidateData) => {
+    console.log('new-ice-candidate', data);
+    // Send the ice candidate to the target
+  });
 });
 
 io.listen(3000);
+
+type VideoOfferData = {
+  type: 'video-offer';
+  name: string; // Sender's name
+  target: string; // // Person receiving the description
+  sdp: string; // Description to send
+};
+
+type VideoAnswerData = {
+  type: 'video-answer';
+  name: string; // Sender's name
+  target: string; // // Person receiving the description
+  sdp: string; // Description to send
+};
+
+type IceCandidateData = {
+  type: 'new-ice-candidate';
+  target: string; // Person receiving the description
+  candidate: string; // The SDP candidate string
+};
